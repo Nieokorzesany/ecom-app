48e7354ad481830dae5031fe0764e94c1cb59854
Piotr Wlazeł <pancu1222@gmail.com>
2019-10-04 19:38:59 +0200

    broken homepage into smaller components and finished styling menu-item component

--- src/HomepageComponent.js
+++ src/HomepageComponent.js
@@ -1,40 +1,12 @@
 import React from "react";
+import "./HomepageComponent.scss";
+import MenuItem from "./components/menu-item/Menu-item";
+import Directory from "./components/directory/Directory";
 
 export default function HomepageComponent() {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Hats</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Jackets</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Pants</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Womens</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Men</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 }
